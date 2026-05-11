75f60eb5a2a161d9822ff59703dbd2f571f0f7cd
hannahcomiskey <hannahcomiskey68@gmail.com>
2026-05-11 12:41:21 +1000

    Updates

--- R/run_convolution.R
+++ R/run_convolution.R
@@ -5,11 +5,124 @@
 #' @param point A true/false indicator to denote whether you are using point estimates (point=TRUE) or posterior samples (point=FALSE) of the Poisson parameters.
 #' @param z_values A grid of evaluation points. If the family is in the Beta-family, this can be left NULL and it defaults to a vector of length 1000 over the unit interval.
 #' @param ... Parameters specific to the parameter family. See "Poisson_convolution", "Beta_convolution", "ZIB_convolution", and "ZOIB_convolution" for family-specific details.
-
 #' @details
 #' A wrapper function for calculating Monte Carlo estimates for the aggregate
 #' density Z using point estimates or posterior samples of lambda.
+#' @examples
+#' ## ---------------------------------------------------------------
+#' ## Example 1: Poisson convolution using point estimates
+#' ## ---------------------------------------------------------------
 #'
+#' set.seed(123)
+#'
+#' ## Hierarchy:
+#' ## State-by-sex -> State -> National
+#'
+#' groups <- list(
+#'   NSW = c("NSW_Male", "NSW_Female"),
+#'   VIC = c("VIC_Male", "VIC_Female"),
+#'   State = c("NSW", "VIC"),
+#'   National = c("AUS")
+#' )
+#'
+#' ## Evaluation grid
+#' z_values <- 0:60
+#'
+#' ## Point estimates for lambda
+#' lambda_list <- list(
+#'   c(14, 12),
+#'   c(13, 13),
+#'   c(26, 26),
+#'   52
+#' )
+#'
+#' ## Run convolution
+#' dens_pois <- run_convolution(
+#'   family = "Poisson",
+#'   groups = groups,
+#'   point = TRUE,
+#'   z_values = z_values,
+#'   lambda_list = lambda_list
+#' )
+#'
+#' ## Inspect results
+#' head(dens_pois[[1]])
+#'
+#'
+#' ## ---------------------------------------------------------------
+#' ## Example 2: Beta convolution using point estimates
+#' ## ---------------------------------------------------------------
+#'
+#' set.seed(123)
+#'
+#' groups_beta <- list(
+#'   State = c("NSW", "VIC"),
+#'   National = c("AUS")
+#' )
+#'
+#' alpha_list <- list(
+#'   c(2, 5),
+#'   7
+#' )
+#'
+#' beta_list <- list(
+#'   c(6, 3),
+#'   4
+#' )
+#'
+#' weights_list <- list(
+#'   c(0.4, 0.6),
+#'   1
+#' )
+#'
+#' dens_beta <- run_convolution(
+#'   family = "Beta",
+#'   groups = groups_beta,
+#'   point = TRUE,
+#'   z_values = NULL,
+#'   alpha_list = alpha_list,
+#'   beta_list = beta_list,
+#'   weights_list = weights_list,
+#'   n_draws = 500,
+#'   n_sims = 50
+#' )
+#'
+#' ## Inspect results
+#' head(dens_beta[[1]])
+#'
+#'
+#' ## ---------------------------------------------------------------
+#' ## Example 3: Zero-one-inflated Beta convolution
+#' ## ---------------------------------------------------------------
+#'
+#' set.seed(123)
+#'
+#' zoi_list <- list(
+#'   c(0.10, 0.15),
+#'   0.05
+#' )
+#'
+#' coi_list <- list(
+#'   c(0.05, 0.10),
+#'   0.02
+#' )
+#'
+#' dens_zoib <- run_convolution(
+#'   family = "ZOIB",
+#'   groups = groups_beta,
+#'   point = TRUE,
+#'   z_values = NULL,
+#'   alpha_list = alpha_list,
+#'   beta_list = beta_list,
+#'   zoi_list = zoi_list,
+#'   coi_list = coi_list,
+#'   weights_list = weights_list,
+#'   n_draws = 500,
+#'   n_sims = 50
+#' )
+#'
+#' ## Inspect results
+#' head(dens_zoib[[1]])
 #' @return The aggregate density Z over a grid of values.
 #' @export
 #'
